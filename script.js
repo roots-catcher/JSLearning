1f199851508156f66650c91dae30a8a9dcf3a446
roots_catcher <yurkor4@gmail.com>
2022-11-02 19:59:56 +0300

    practic

--- script.js
+++ script.js
@@ -3,20 +3,28 @@
 console.log("Hello, i see you!");
 
 //alert("Hey");
-
 //const result = confirm("Are you here?");
-
 //console.log(result);
-
 //const answer = prompt("Answer?", "No");
-
 // const answers = [];
-
 // answers[0] = prompt("What's youre name?");
 // answers[1] = prompt("How are you?");
-
 // document.write(answers);
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
 
-const category = 'toys';
+let numberOfFilms = prompt("How much films you see?", "");
+let lastFilm = prompt("What last film you see?","");
+let rating = prompt("How much?", "");
 
-console.log(`https://someurl.com/${category}/5`);
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+
+};
+personalMovieDB.movies.lastFilm = rating;
+
+console.log(personalMovieDB.movies);
