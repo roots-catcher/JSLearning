cbdcc51834b09b853ffd548d41d9a7220ce556ee
roots_catcher <yurkor4@gmail.com>
2022-11-02 19:24:44 +0300

    my first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+"Use strict";
+
+console.log("Hello, i see you!");
+
+//alert("Hey");
+
+//const result = confirm("Are you here?");
+
+//console.log(result);
+
+//const answer = prompt("Answer?", "No");
+
+// const answers = [];
+
+// answers[0] = prompt("What's youre name?");
+// answers[1] = prompt("How are you?");
+
+// document.write(answers);
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
